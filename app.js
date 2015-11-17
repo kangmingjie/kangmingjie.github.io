e19351fe0db3d6a49e1e7863acf54d759153b757
Jimmy <Jimmy>
2015-11-16 23:42:21 -0700

    added Chinese to typed.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(function(){
       var typedJS = $(".typed-js").typed({
-            strings: ["an Innovator", "a CSS Wizard", "a Digital Warrior", "a Code Ninja", "a Master of Angular", "a Mongo Magistrate", "a Git Gladiator"],
+            strings: ["an Innovator", "a CSS Wizard", "a Digital Warrior", "a Code Ninja", "a Master of Angular", "忍着"， "a Mongo Magistrate", "a Git Gladiator"],
             typeSpeed: 110,
             loop: true,
             backDelay: 750 
